0ddd5b59a1a70b4345a1b085390c3e63ab499604
Cristian Cruz <cristiancy96@gmail.com>
2021-03-17 17:27:18 -0500

    Update LocationUpdatesService.kt

--- android/src/main/kotlin/com/almoullim/background_location/LocationUpdatesService.kt
+++ android/src/main/kotlin/com/almoullim/background_location/LocationUpdatesService.kt
@@ -42,7 +42,7 @@ class LocationUpdatesService : Service() {
         internal val ACTION_BROADCAST = "$PACKAGE_NAME.broadcast"
         internal val EXTRA_LOCATION = "$PACKAGE_NAME.location"
         private val EXTRA_STARTED_FROM_NOTIFICATION = "$PACKAGE_NAME.started_from_notification"
-        var UPDATE_INTERVAL_IN_MILLISECONDS: Long = 1000
+        var UPDATE_INTERVAL_IN_MILLISECONDS: Long = 60000
         private val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
         private val NOTIFICATION_ID = 12345678
         private lateinit var broadcastReceiver: BroadcastReceiver
