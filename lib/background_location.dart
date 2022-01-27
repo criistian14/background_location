1a0b57d7d3f4dc850ceabefd94dc24a00e52c905
Christian <criistian-14@hotmail.com>
2022-01-26 22:40:52 -0500

    upgrade flutter apis and add event channel

--- lib/background_location.dart
+++ lib/background_location.dart
@@ -1,35 +1,43 @@
 import 'dart:async';
-import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'dart:io' show Platform;
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 /// BackgroundLocation plugin to get background
-/// lcoation updates in iOS and Android
+/// location updates in iOS and Android
 class BackgroundLocation {
-  // The channel to be used for communication.
-  // This channel is also refrenced inside both iOS and Abdroid classes
+  // The channels to be used for communication.
+  // This channels are also referenced inside both iOS and Android classes
+  static const String _pluginId = "almoullim.com";
   static const MethodChannel _channel =
-      const MethodChannel('almoullim.com/background_location');
+      const MethodChannel('$_pluginId/background_location');
+  static const EventChannel _eventChannel =
+      const EventChannel('$_pluginId/background_location_stream');
+
+  static StreamSubscription _streamSubscriptionUpdates;
 
   /// Stop receiving location updates
   static stopLocationService() async {
+    _streamSubscriptionUpdates?.cancel();
     return await _channel.invokeMethod("stop_location_service");
   }
 
   /// Start receiving location updated
   static startLocationService({double distanceFilter = 0.0}) async {
-    return await _channel.invokeMethod("start_location_service", <String, dynamic>{
-      "distance_filter": distanceFilter
+    _channel.setMethodCallHandler((MethodCall methodCall) async {
+      print("${methodCall.method}");
     });
+    return await _channel.invokeMethod("start_location_service",
+        <String, dynamic>{"distance_filter": distanceFilter});
   }
 
-  static setAndroidNotification({String title, String message, String icon}) async {
+  static setAndroidNotification(
+      {String title, String message, String icon}) async {
     if (Platform.isAndroid) {
-      return await _channel.invokeMethod("set_android_notification", <String, dynamic>{
-        "title": title,
-        "message": message,
-        "icon": icon
-      });
+      return await _channel.invokeMethod("set_android_notification",
+          <String, dynamic>{"title": title, "message": message, "icon": icon});
     } else {
       //return Promise.resolve();
     }
@@ -45,20 +53,22 @@ class BackgroundLocation {
     }
   }
 
-
   /// Get the current location once.
   Future<Location> getCurrentLocation() async {
     Completer<Location> completer = Completer();
 
     Location _location = Location();
     await getLocationUpdates((location) {
-      _location.latitude = location.latitude;
-      _location.longitude = location.longitude;
-      _location.accuracy = location.accuracy;
-      _location.altitude = location.altitude;
-      _location.bearing = location.bearing;
-      _location.speed = location.speed;
-      _location.time = location.time;
+      _location = _location.copyWith(
+        latitude: location.latitude,
+        longitude: location.longitude,
+        accuracy: location.accuracy,
+        altitude: location.altitude,
+        bearing: location.bearing,
+        speed: location.speed,
+        time: location.time,
+      );
+
       completer.complete(_location);
     });
 
@@ -74,7 +84,7 @@ class BackgroundLocation {
       }
     } else if (await Permission.locationWhenInUse.isDenied ||
         await Permission.locationWhenInUse.isPermanentlyDenied ||
-        await Permission.locationWhenInUse.isRestricted ) {
+        await Permission.locationWhenInUse.isRestricted) {
       if (onDenied != null) {
         onDenied();
       }
@@ -87,55 +97,56 @@ class BackgroundLocation {
     return permission;
   }
 
-  /// Register a function to recive location updates as long as the location
+  /// Register a function to receive location updates as long as the location
   /// service has started
   static getLocationUpdates(Function(Location) location) {
-    // add a handler on the channel to recive updates from the native classes
-    _channel.setMethodCallHandler((MethodCall methodCall) async {
-      if (methodCall.method == "location") {
-        Map locationData = Map.from(methodCall.arguments);
-        // Call the user passed function
-        location(
-          Location(
-            latitude: locationData["latitude"],
-            longitude: locationData["longitude"],
-            altitude: locationData["altitude"],
-            accuracy: locationData["accuracy"],
-            bearing: locationData["bearing"],
-            speed: locationData["speed"],
-            time: locationData["time"],
-            isMock: locationData["is_mock"]
-          ),
-        );
-      }
+    _streamSubscriptionUpdates =
+        _eventChannel.receiveBroadcastStream().listen((event) {
+      Map locationData = Map.from(event);
+
+      // Call the user passed function
+      location(
+        Location(
+          latitude: locationData["latitude"],
+          longitude: locationData["longitude"],
+          altitude: locationData["altitude"],
+          accuracy: locationData["accuracy"],
+          bearing: locationData["bearing"],
+          speed: locationData["speed"],
+          time: locationData["time"],
+          isMock: locationData["is_mock"],
+        ),
+      );
     });
   }
 }
 
-/// An object containing infromation
+/// An object containing information
 /// about the user current location
+@immutable
 class Location {
-  Location(
-      {this.longitude,
-      this.latitude,
-      this.altitude,
-      this.accuracy,
-      this.bearing,
-      this.speed,
-      this.time,
-      this.isMock});
+  const Location({
+    this.longitude,
+    this.latitude,
+    this.altitude,
+    this.accuracy,
+    this.bearing,
+    this.speed,
+    this.time,
+    this.isMock,
+  });
 
-  double latitude;
-  double longitude;
-  double altitude;
-  double bearing;
-  double accuracy;
-  double speed;
-  double time;
-  bool isMock;
-  
-  toMap() {
-    var obj = {
+  final double latitude;
+  final double longitude;
+  final double altitude;
+  final double bearing;
+  final double accuracy;
+  final double speed;
+  final double time;
+  final bool isMock;
+
+  Map<String, dynamic> toMap() {
+    return {
       'latitude': this.latitude,
       'longitude': this.longitude,
       'altitude': this.altitude,
@@ -145,6 +156,27 @@ class Location {
       'time': this.time,
       'is_mock': this.isMock
     };
-    return obj;
-  } 
+  }
+
+  Location copyWith({
+    double latitude,
+    double longitude,
+    double altitude,
+    double bearing,
+    double accuracy,
+    double speed,
+    double time,
+    bool isMock,
+  }) {
+    return Location(
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
+      altitude: altitude ?? this.altitude,
+      bearing: bearing ?? this.bearing,
+      accuracy: accuracy ?? this.accuracy,
+      speed: speed ?? this.speed,
+      time: time ?? this.time,
+      isMock: isMock ?? this.isMock,
+    );
+  }
 }
