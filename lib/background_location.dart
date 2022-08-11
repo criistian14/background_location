81339723a5eec5affc62993bde58b7248c3f5d93
Cristian Cruz <cristian.cruz@quick.com.co>
2022-08-11 03:02:57 -0500

    Update change

--- lib/background_location.dart
+++ lib/background_location.dart
@@ -2,38 +2,38 @@ import 'dart:async';
 import 'dart:io' show Platform;
 import 'dart:math';
 
-import 'package:equatable/equatable.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 /// BackgroundLocation plugin to get background
 /// location updates in iOS and Android
 class BackgroundLocation {
-  // The channels to be used for communication.
-  // This channels are also referenced inside both iOS and Android classes
-  static const String _pluginId = "almoullim.com";
+  // The channel to be used for communication.
+  // This channel is also refrenced inside both iOS and Abdroid classes
   static const MethodChannel _channel =
-      const MethodChannel('$_pluginId/background_location');
-  static const EventChannel _eventChannel =
-      const EventChannel('$_pluginId/background_location_stream');
+      MethodChannel('com.almoullim.background_location/methods');
 
   /// Stop receiving location updates
   static stopLocationService() async {
-    return await _channel.invokeMethod("stop_location_service");
+    return await _channel.invokeMethod('stop_location_service');
   }
 
   /// Start receiving location updated
-  static startLocationService({double distanceFilter = 0.0}) async {
-    return await _channel.invokeMethod("start_location_service",
-        <String, dynamic>{"distance_filter": distanceFilter});
+  static startLocationService(
+      {double distanceFilter = 0.0,
+      bool forceAndroidLocationManager = false}) async {
+    return await _channel
+        .invokeMethod('start_location_service', <String, dynamic>{
+      'distance_filter': distanceFilter,
+      'force_location_manager': forceAndroidLocationManager
+    });
   }
 
   static setAndroidNotification(
-      {String title, String message, String icon}) async {
+      {String? title, String? message, String? icon}) async {
     if (Platform.isAndroid) {
-      return await _channel.invokeMethod("set_android_notification",
-          <String, dynamic>{"title": title, "message": message, "icon": icon});
+      return await _channel.invokeMethod('set_android_notification',
+          <String, dynamic>{'title': title, 'message': message, 'icon': icon});
     } else {
       //return Promise.resolve();
     }
@@ -41,53 +41,35 @@ class BackgroundLocation {
 
   static setAndroidConfiguration(int interval) async {
     if (Platform.isAndroid) {
-      return await _channel.invokeMethod("set_configuration", <String, dynamic>{
-        "interval": interval.toString(),
+      return await _channel.invokeMethod('set_configuration', <String, dynamic>{
+        'interval': interval.toString(),
       });
     } else {
       //return Promise.resolve();
     }
   }
 
-  // Get the current location
-  static Future<Location> getCurrentLocation() async {
-    try {
-      final response = await _channel.invokeMethod(
-        "get_current_location",
-      );
+  /// Get the current location once.
+  Future<Location> getCurrentLocation() async {
+    var completer = Completer<Location>();
 
-      Map locationData = Map.from(response);
+    var _location = Location();
+    await getLocationUpdates((location) {
+      _location.latitude = location.latitude;
+      _location.longitude = location.longitude;
+      _location.accuracy = location.accuracy;
+      _location.altitude = location.altitude;
+      _location.bearing = location.bearing;
+      _location.speed = location.speed;
+      _location.time = location.time;
+      completer.complete(_location);
+    });
 
-      return Location(
-        latitude: locationData["latitude"],
-        longitude: locationData["longitude"],
-        altitude: locationData["altitude"],
-        accuracy: locationData["accuracy"],
-        bearing: locationData["bearing"],
-        speed: locationData["speed"],
-        time: locationData["time"],
-        isMock: locationData["is_mock"],
-      );
-    } catch (e) {
-      return null;
-    }
+    return completer.future;
   }
 
   /// Ask the user for location permissions
-  static getPermissions({Function onGranted, Function onDenied}) async {
-    await Permission.locationWhenInUse.request();
-    if (await Permission.locationWhenInUse.isGranted) {
-      if (onGranted != null) {
-        onGranted();
-      }
-    } else if (await Permission.locationWhenInUse.isDenied ||
-        await Permission.locationWhenInUse.isPermanentlyDenied ||
-        await Permission.locationWhenInUse.isRestricted) {
-      if (onDenied != null) {
-        onDenied();
-      }
-    }
-  }
+  static getPermissions() async {}
 
   // /// Check what the current permissions status is
   // static Future<PermissionStatus> checkPermissions() async {
@@ -95,25 +77,26 @@ class BackgroundLocation {
   //   return permission;
   // }
 
-  /// Register a function to receive location updates as long as the location
+  /// Register a function to recive location updates as long as the location
   /// service has started
   static getLocationUpdates(Function(Location) location) {
-    _eventChannel.receiveBroadcastStream().listen((event) {
-      Map locationData = Map.from(event);
-
-      // Call the user passed function
-      location(
-        Location(
-          latitude: locationData["latitude"],
-          longitude: locationData["longitude"],
-          altitude: locationData["altitude"],
-          accuracy: locationData["accuracy"],
-          bearing: locationData["bearing"],
-          speed: locationData["speed"],
-          time: locationData["time"],
-          isMock: locationData["is_mock"],
-        ),
-      );
+    // add a handler on the channel to recive updates from the native classes
+    _channel.setMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'location') {
+        var locationData = Map.from(methodCall.arguments);
+        // Call the user passed function
+        location(
+          Location(
+              latitude: locationData['latitude'],
+              longitude: locationData['longitude'],
+              altitude: locationData['altitude'],
+              accuracy: locationData['accuracy'],
+              bearing: locationData['bearing'],
+              speed: locationData['speed'],
+              time: locationData['time'],
+              isMock: locationData['is_mock']),
+        );
+      }
     });
   }
 
@@ -124,10 +107,10 @@ class BackgroundLocation {
   /// supplied coordinates [startLatitude], [startLongitude], [endLatitude] and
   /// [endLongitude] should be supplied in degrees.
   static double distanceBetween({
-    @required double startLatitude,
-    @required double startLongitude,
-    @required double endLatitude,
-    @required double endLongitude,
+    required double startLatitude,
+    required double startLongitude,
+    required double endLatitude,
+    required double endLongitude,
   }) {
     var earthRadius = 6378137.0;
     var dLat = _toRadians(endLatitude - startLatitude);
@@ -169,143 +152,40 @@ class BackgroundLocation {
       return false;
     }
   }
-
-  /// Returns a [Future] indicating if the user allows the App to access
-  /// the device's location.
-  static Future<LocationPermission> checkPermission() async {
-    try {
-      final int permission = await _channel.invokeMethod('check_permission');
-
-      return permission.toLocationPermission();
-    } on PlatformException catch (e) {
-      return null;
-    }
-  }
-
-  /// Returns a [Future] containing a [bool] value indicating whether location
-  /// services are enabled on the device.
-  static Future<bool> isLocationServiceEnabled() async {
-    try {
-      return await _channel.invokeMethod('is_location_service_enabled');
-    } on PlatformException catch (e) {
-      return null;
-    }
-  }
 }
 
-/// An object containing information
 /// about the user current location
-@immutable
-class Location extends Equatable {
-  const Location({
-    this.longitude,
-    this.latitude,
-    this.altitude,
-    this.accuracy,
-    this.bearing,
-    this.speed,
-    this.time,
-    this.isMock,
-  });
+class Location {
+  double? latitude;
+  double? longitude;
+  double? altitude;
+  double? bearing;
+  double? accuracy;
+  double? speed;
+  double? time;
+  bool? isMock;
 
-  final double latitude;
-  final double longitude;
-  final double altitude;
-  final double bearing;
-  final double accuracy;
-  final double speed;
-  final double time;
-  final bool isMock;
+  Location(
+      {@required this.longitude,
+      @required this.latitude,
+      @required this.altitude,
+      @required this.accuracy,
+      @required this.bearing,
+      @required this.speed,
+      @required this.time,
+      @required this.isMock});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'latitude': this.latitude,
-      'longitude': this.longitude,
-      'altitude': this.altitude,
-      'bearing': this.bearing,
-      'accuracy': this.accuracy,
-      'speed': this.speed,
-      'time': this.time,
-      'is_mock': this.isMock
+  toMap() {
+    var obj = {
+      'latitude': latitude,
+      'longitude': longitude,
+      'altitude': altitude,
+      'bearing': bearing,
+      'accuracy': accuracy,
+      'speed': speed,
+      'time': time,
+      'is_mock': isMock
     };
-  }
-
-  Location copyWith({
-    double latitude,
-    double longitude,
-    double altitude,
-    double bearing,
-    double accuracy,
-    double speed,
-    double time,
-    bool isMock,
-  }) {
-    return Location(
-      latitude: latitude ?? this.latitude,
-      longitude: longitude ?? this.longitude,
-      altitude: altitude ?? this.altitude,
-      bearing: bearing ?? this.bearing,
-      accuracy: accuracy ?? this.accuracy,
-      speed: speed ?? this.speed,
-      time: time ?? this.time,
-      isMock: isMock ?? this.isMock,
-    );
-  }
-
-  @override
-  List<Object> get props => [
-        latitude,
-        longitude,
-        altitude,
-        bearing,
-        accuracy,
-        speed,
-        time,
-        isMock,
-      ];
-}
-
-/// Represent the possible location permissions.
-enum LocationPermission {
-  /// This is the initial state on both Android and iOS, but on Android the
-  /// user can still choose to deny permissions, meaning the App can still
-  /// request for permission another time.
-  denied,
-
-  /// Permission to access the device's location is permenantly denied. When
-  /// requestiong permissions the permission dialog will not been shown until
-  /// the user updates the permission in the App settings.
-  deniedForever,
-
-  /// Permission to access the device's location is allowed only while
-  /// the App is in use.
-  whileInUse,
-
-  /// Permission to access the device's location is allowed even when the
-  /// App is running in the background.
-  always
-}
-
-/// Provides extension methods on the LocationAccuracy enum.
-extension IntergerExtensions on int {
-  /// Tries to convert the int value to a LocationPermission enum value.
-  ///
-  /// Throws an InvalidPermissionException if the int value cannot be
-  /// converted to a LocationPermission.
-  LocationPermission toLocationPermission() {
-    switch (this) {
-      case 0:
-        return LocationPermission.denied;
-      case 1:
-        return LocationPermission.deniedForever;
-      case 2:
-        return LocationPermission.whileInUse;
-      case 3:
-        return LocationPermission.always;
-      default:
-        throw Exception(
-          'Unable to convert the value "$this" into a LocationPermission.',
-        );
-    }
+    return obj;
   }
 }
